37f63f749e9007a31e9c254ef1b6238c58ffa490
Bui Bao Hoang <89177013+buibaohoang06@users.noreply.github.com>
2022-04-26 21:56:20 +0700

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -2,10 +2,17 @@ from datetime import datetime
 
 #defining the amount list
 daily = []
+thirtyone = [1, 3, 5, 7, 8, 10, 12]
+thirty = [2, 4, 6, 9, 11]
+month = datetime.now().strftime("%m")
+if month in thirtyone:
+	count = 31
+elif month in thirty:
+	count = 30
 def delete():
-	thirtyone = [1, 3, 5, 7, 8, 10, 12]
-	thirty = [2, 4, 6, 9, 11]
-	month = datetime.now().strftime("%m")
+	global thirtyone
+	global thirty
+	global month
 	if month in thirtyone and len(daily) == 31:
 		daily = []
 	elif month in thirty and len(daily) == 30:
@@ -23,36 +30,45 @@ def checker(amount):
 	return status
 def getAmount():
 	global daily
+	sum = 0
+	global count
 	#check the amount of number in array
 	delete()
 	while True:
 		try:
-			amount = int(input("Type amount of water you used today."))
+			amount = int(input("Input amount of Water: "))
+			if len(daily) >= 1:
+				daily.append(amount)
+				#calculate the sum within the daily
+				for i in daily:
+					sum = sum + i
+				#sum 
+				results = sum + (daily[-1] * count)
+				if amount < sum:
+					conclusion = f"{sum - amount}L less than predicted"
+				elif amount > sum:
+					conclusion = f"{amount - sum}L more than predicted"
+				else:
+					conclusion = "as predicted"
+				#reduce by 1 everyday
+				count = count - 1
+				#calculate the percentage
+				percentage = daily[-1] / daily[-2] * 100
+				if percentage > 100:
+					status = "more than"
+				elif percentage < 100:
+					percentage = 100 - percentage
+					status = "less than"
+				else:
+					status = "the same as"
+				print(f"Your usage today was {percentage}% {status} yesterday, {conclusion}")
+			else: 
+				daily.append(amount)
 		except TypeError:
 			print("Variable entered is not integer")
+		
+
 	
-		if checker(amount=amount) == "Excellent":
-			print("Your water usage for today is great!")
-			daily.append(amount)
-			break
-		elif checker(amount=amount) == "Poor":
-			print("The amount you entered as exceeded the allowed paramenters. Would you like to hear some advice?")
-			while True:
-				command = input("%> ")
-				if command.lower() == "yes" or command.lower() == "y":
-					#enter scenario here
-					print("Help")
-					break
-				elif command.lower() == "no" or command.lower() == "n":
-					#enter scenario here
-					print("No help")
-					break
-				else:
-					print("Command error, try again!")
-			daily.append(amount)
-			print(daily)
-
-
 if __name__ == "__main__":
 	while True:
 		getAmount()
